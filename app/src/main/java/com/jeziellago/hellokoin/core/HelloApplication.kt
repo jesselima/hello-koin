716b1bd75a4551045c136f14c048b73c7592803c
Jesse Lima <jesselima.tech@gmail.com>
2021-08-09 19:30:26 -0300

    Add picasso single instance for cache.

--- app/src/main/java/com/jeziellago/hellokoin/core/HelloApplication.kt
+++ app/src/main/java/com/jeziellago/hellokoin/core/HelloApplication.kt
@@ -2,6 +2,8 @@ package com.jeziellago.hellokoin.core
 
 import android.app.Application
 import com.jeziellago.hellokoin.features.hello.di.initHelloDependencies
+import com.squareup.picasso.OkHttp3Downloader
+import com.squareup.picasso.Picasso
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -19,5 +21,16 @@ class HelloApplication : Application() {
             androidContext(this@HelloApplication)
         }
         initHelloDependencies()
+        setupPicassoInstance()
     }
+
+    private fun setupPicassoInstance() {
+        val picassoBuilder = Picasso.Builder(this)
+        picassoBuilder.downloader(OkHttp3Downloader(this, Long.MAX_VALUE))
+        val picasso = picassoBuilder.build()
+        picasso.setIndicatorsEnabled(true)
+        picasso.isLoggingEnabled = true
+        Picasso.setSingletonInstance(picasso)
+    }
+
 }
